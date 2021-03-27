ae4f893a5e95bec482975f35335ff4a05a3c802d
Emil <inferngeek@gmail.com>
2021-03-27 23:33:04 +0300

    более грамотно оформил вкид

--- src/__main__.py
+++ src/__main__.py
@@ -4,10 +4,13 @@ from vkbottle.bot import Bot, Message
 import toml
 import BotData
 
-cfg = toml.load('config.toml')
-bot = Bot(cfg['auth_data']['token'])
+def main():
+    bot = Bot(BotData.data.cfg['auth_data']['token'])
 
-for bp in BotData.bps:
-    bp.load(bot)
+    for bp in BotData.bps:
+        bp.load(bot)
 
-bot.run_forever()
+    bot.run_forever()
+
+if __name__ == '__main__':
+    main()
