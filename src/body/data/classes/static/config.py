dec1ccdadff02e74c0e86a10d64713e07efad609
Emil <inferngeek@gmail.com>
2021-04-16 14:24:48 +0300

    ?? idk

--- src/body/data/classes/static/config.py
+++ src/body/data/classes/static/config.py
@@ -1,13 +1,14 @@
 import json
+import os
+from src.body.data.storages.constants import CONFIGURATION_PATH
 
-# CONSTANTS
-CONFIGURATION_PATH = 'body\\data\\storages\\config.json' # Path to configuration file
 
 class Configuration:
     """Class for read configuration file"""
 
     def __init__(self):
-        self.json_variable = json.load(open(CONFIGURATION_PATH, 'r'))
-        
+        with open(CONFIGURATION_PATH, 'r') as config_file:
+            self.json_variable = json.load(config_file)
+
     def __call__(self):
         return self.json_variable
